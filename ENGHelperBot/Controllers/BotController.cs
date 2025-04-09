18a02c29e8ba872f3e5985898c72dd364af5c718
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-09 14:45:11 +0300

    Fix app launch issues

--- ENGHelperBot/Controllers/BotController.cs
+++ ENGHelperBot/Controllers/BotController.cs
@@ -4,34 +4,39 @@ using Telegram.Bot.Requests.Abstractions;
 using Telegram.Bot.Types;
 using Telegram.Bot;
 using ENGHelperBot.Services;
+using Telegram.Bot.Polling;
 
 namespace ENGHelperBot.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class BotController(IOptions<BotConfiguration> Config) : ControllerBase
+public class BotController(BotConfiguration botConfig) : ControllerBase
 {
+    private readonly BotConfiguration _botConfig = botConfig;
+
     [HttpGet("setWebhook")]
     public async Task<string> SetWebHook([FromServices] ITelegramBotClient bot, CancellationToken ct)
     {
-        var webhookUrl = Config.Value.BotWebhookUrl.AbsoluteUri;
-        await bot.SetWebhook(webhookUrl, allowedUpdates: [], secretToken: Config.Value.SecretToken, cancellationToken: ct);
+        var webhookUrl = _botConfig.BotWebhookUrl.AbsoluteUri;
+        await bot.SetWebhook(webhookUrl, allowedUpdates: [], secretToken: _botConfig.SecretToken, cancellationToken: ct);
         return $"Webhook set to {webhookUrl}";
     }
 
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] Update update, [FromServices] ITelegramBotClient bot, [FromServices] UpdateHandler handleUpdateService, CancellationToken ct)
     {
-        if (Request.Headers["X-Telegram-Bot-Api-Secret-Token"] != Config.Value.SecretToken)
-            return Forbid();
+        //if (Request.Headers["X-Telegram-Bot-Api-Secret-Token"] != Config.Value.SecretToken)
+        //    return Forbid();
+
         try
         {
             await handleUpdateService.HandleUpdateAsync(bot, update, ct);
         }
         catch (Exception exception)
         {
-            await handleUpdateService.HandleErrorAsync(bot, exception, Telegram.Bot.Polling.HandleErrorSource.HandleUpdateError, ct);
+            await handleUpdateService.HandleErrorAsync(bot, exception, HandleErrorSource.HandleUpdateError, ct);
         }
+
         return Ok();
     }
 }
