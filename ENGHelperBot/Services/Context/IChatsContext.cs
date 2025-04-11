db37a40369c6ee90bd8a7ec219c836ff06abc146
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-11 23:09:29 +0300

    Add chats context class

---
+++ ENGHelperBot/Services/Context/IChatsContext.cs
@@ -0,0 +1,9 @@
+﻿using ENGHelperBot.Services.Command;
+
+namespace ENGHelperBot.Services.Context;
+
+public interface IChatsContext
+{
+    ICommandHandler? GetFollowingCommand(long chatId);
+    void SetFollowingCommand(long chatId, ICommandHandler command);
+}
