9cc994b0a721a58e11c39268ead5c974df2c7e66
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-11 23:10:12 +0300

    Add ICommandHandler interface

---
+++ ENGHelperBot/Services/Command/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ENGHelperBot.Services.Command;
+
+public interface ICommandHandler
+{
+    Task<Message> HandleAsync(ITelegramBotClient bot, Update update); 
+}
