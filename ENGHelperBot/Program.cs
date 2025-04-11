96b6e0f6583eeaa1362fdde6525b483e679dcf83
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-11 23:23:16 +0300

    Add necessary dependencies for new classes

--- ENGHelperBot/Program.cs
+++ ENGHelperBot/Program.cs
@@ -1,6 +1,8 @@
 using ENGHelperBot;
 using ENGHelperBot.Extensions;
 using ENGHelperBot.Services;
+using ENGHelperBot.Services.Command;
+using ENGHelperBot.Services.Context;
 using Telegram.Bot;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,9 @@ builder.Services.AddHttpClient("tgwebhook")
     .RemoveAllLoggers()
     .AddTypedClient<ITelegramBotClient>((httpClient, sp) => new TelegramBotClient(sp.GetRequiredService<BotConfiguration>().BotToken, httpClient));
 builder.Services.AddSingleton<UpdateHandler>();
+builder.Services.AddSingleton<IChatsContext, ChatsContext>();
+builder.Services.AddSingleton<ICommandProvider, CommandProvider>();
+builder.Services.AddScoped<ICommandHandler, StartCommand>();
 builder.Services.ConfigureTelegramBotMvc();
 
 builder.Services.AddControllers();
