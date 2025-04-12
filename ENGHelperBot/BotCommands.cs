53103c1d5ad4e9ada38fcb0b0e60a7debb0f6f50
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-12 23:42:49 +0300

    Add class with commands literals as constants

---
+++ ENGHelperBot/BotCommands.cs
@@ -0,0 +1,9 @@
+﻿namespace ENGHelperBot;
+
+public static class BotCommands
+{
+    public const string Start = "/start";
+    public const string MyDictionaries = "📚 Мои словари";
+    public const string AddWord = "➕ Добавить слово";
+    public const string TakeTest = "📝 Пройти тест";
+}
