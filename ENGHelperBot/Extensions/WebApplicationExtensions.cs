8a5ad7d8a927c090ef1e65db75299a9d25312ad8
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-14 17:48:27 +0300

    Add method for auto migrations

---
+++ ENGHelperBot/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ENGHelperBot.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public async static void MigrateAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+        if (pendingMigrations.Any()) await dbContext.Database.MigrateAsync();
+    }
+}
