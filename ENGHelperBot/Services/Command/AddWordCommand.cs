1407c2e62d1011ad762caada2982a2afdace7853
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-15 13:03:55 +0300

    Add stubs of main menu classes

---
+++ ENGHelperBot/Services/Command/AddWordCommand.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ENGHelperBot.Services.Command;
+
+public class AddWordCommand : ICommandHandler
+{
+    public Task<Message> HandleAsync(ITelegramBotClient bot, Update update)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ENGHelperBot/Services/Command/MyDictionariesCommand.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ENGHelperBot.Services.Command;
+
+public class MyDictionariesCommand : ICommandHandler
+{
+    public Task<Message> HandleAsync(ITelegramBotClient bot, Update update)
+    {
+        throw new NotImplementedException();
+    }
+}
