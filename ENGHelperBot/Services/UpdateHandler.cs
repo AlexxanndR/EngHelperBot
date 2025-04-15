2e0446a1d0f257f57d1d1ae22fbddc1da9c263ab
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-15 13:06:03 +0300

    Add first UpdateHandler logic

--- ENGHelperBot/Services/UpdateHandler.cs
+++ ENGHelperBot/Services/UpdateHandler.cs
@@ -1,35 +1,44 @@
-﻿using Telegram.Bot;
+﻿using ENGHelperBot.Services.Command.Provider;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 
 namespace ENGHelperBot.Services;
 
-public class UpdateHandler(ITelegramBotClient bot) : IUpdateHandler
+public class UpdateHandler(ICommandProvider commandProvider) : IUpdateHandler
 {
-    public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+    private readonly ICommandProvider _commandProvider = commandProvider;
+
+    public async Task HandleErrorAsync(ITelegramBotClient bot, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
     {
         if (exception is RequestException)
             await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
     }
 
-    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
-        //await (update switch
-        //{
-        //    { Message: { } message } => OnMessage(message),
-        //    { EditedMessage: { } message } => OnMessage(message),
-        //    { CallbackQuery: { } callbackQuery } => OnCallbackQuery(callbackQuery),
-        //    { InlineQuery: { } inlineQuery } => OnInlineQuery(inlineQuery),
-        //    { ChosenInlineResult: { } chosenInlineResult } => OnChosenInlineResult(chosenInlineResult),
-        //    { Poll: { } poll } => OnPoll(poll),
-        //    { PollAnswer: { } pollAnswer } => OnPollAnswer(pollAnswer),
-        //    // ChannelPost:
-        //    // EditedChannelPost:
-        //    // ShippingQuery:
-        //    // PreCheckoutQuery:
-        //    _ => UnknownUpdateHandlerAsync(update)
-        //});
+
+        await (update switch
+        {
+            { Message: { } } => OnMessage(bot, update),
+            _ => UnknownUpdateHandlerAsync(update)
+        });
     }
+
+    private async Task OnMessage(ITelegramBotClient bot, Update update)
+    {
+        if (string.IsNullOrWhiteSpace(update.Message!.Text))
+            return;
+
+        var command = _commandProvider.Get(update.Message.Chat.Id, update.Message.Text);
+        var sentMessage = await command.HandleAsync(bot, update);
+        
+        // TODO: add logging
+    }
+
+    private Task UnknownUpdateHandlerAsync(Update update)
+        => Task.CompletedTask;
 }
