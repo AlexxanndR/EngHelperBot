9427e9dedb8fb5e6d1d04a0d40770ed962770135
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-18 22:54:01 +0300

    Add DictionaryRepository class

---
+++ ENGHelperBot/Services/Repositories/Dictionaries/DictionaryRepository.cs
@@ -0,0 +1,9 @@
+﻿using ENGHelperBot.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENGHelperBot.Services.Repositories.Dictionaries;
+
+public class DictionaryRepository(IDbContextFactory<AppDbContext> contextFactory)
+    : RepositoryBase<Dictionary>(contextFactory), IDictionaryRepository
+{
+}
---
+++ ENGHelperBot/Services/Repositories/Dictionaries/IDictionaryRepository.cs
@@ -0,0 +1,8 @@
+﻿using ENGHelperBot.Data.Entities;
+
+namespace ENGHelperBot.Services.Repositories.Dictionaries;
+
+public interface IDictionaryRepository : IRepositoryBase<Dictionary>
+{
+
+}
