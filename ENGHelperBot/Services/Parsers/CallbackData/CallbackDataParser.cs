2814f9328017e5f0830fa0f3e88cf0765b2a0833
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-20 21:17:27 +0300

    Add base of callback data parser

---
+++ ENGHelperBot/Services/Parsers/CallbackData/CallbackDataParser.cs
@@ -0,0 +1,40 @@
+﻿using ENGHelperBot.Data;
+using Telegram.Bot.Types;
+
+namespace ENGHelperBot.Services.Parsers.CallbackData;
+
+public class CallbackDataParser : ICallbackDataParser
+{
+    public ParsedCallbackData Parse(string callbackData)
+    {
+        var parameters = callbackData.Split(';', StringSplitOptions.RemoveEmptyEntries);
+        
+        if (parameters.Length <= 1) 
+            throw new ArgumentException("Invalid callback data format");
+        
+        var command = parameters[0];
+        return command switch
+        {
+            BotCommands.Previous or BotCommands.Next => ParsePaginationData(callbackData[1..]),
+            _ => throw new ArgumentException($"Unknown command: {command}")
+        };
+    }
+
+    private ParsedCallbackData ParsePaginationData(params string[] parameters)
+    {
+        if (parameters.Length < 2)
+            throw new ArgumentException("Invalid callback data format");
+
+        var dataType = parameters[0] switch
+        {
+            "dict" => PaginationData.DataType.Dictionary,
+            "word" => PaginationData.DataType.Word,
+            _ => throw new ArgumentException($"Unknown data type: {parameters[0]}")
+        };
+        var pageNumber = int.TryParse(parameters[1], out var value) 
+            ? value 
+            : throw new ArgumentException($"Invalid page number: {value}");
+
+        return new ParsedCallbackData { PaginationData = new(dataType, pageNumber) };
+    }
+}
