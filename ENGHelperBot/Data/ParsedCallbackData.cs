e7537529064c7b028ec913b67e26d91d2d37ce20
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-20 21:14:27 +0300

    Add class for parsed callback data

---
+++ ENGHelperBot/Data/ParsedCallbackData.cs
@@ -0,0 +1,11 @@
+﻿namespace ENGHelperBot.Data;
+
+public record ParsedCallbackData
+{
+    public PaginationData? PaginationData { get; set; }
+}
+
+public record PaginationData(PaginationData.DataType Type, int CurrentPage)
+{
+    public enum DataType { Dictionary, Word };
+}
