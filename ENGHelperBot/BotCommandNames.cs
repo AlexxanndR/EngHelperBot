993078011870e239eee93730ed018830b9567f86
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-27 18:38:27 +0300

    Maintain BotCommands.cs

---
+++ ENGHelperBot/BotCommandNames.cs
@@ -0,0 +1,23 @@
+﻿namespace ENGHelperBot;
+
+public static class BotCommandTexts
+{
+    public const string MyDictionaries = "📚 Мои словари";
+    public const string AddWord = "➕ Добавить слово";
+    public const string AddDictionary = "➕ Добавить словарь";
+    public const string TakeTest = "📝 Пройти тест";
+    public const string Back = "Назад";
+    public const string Forward = "Вперед";
+    public const string Return = "Вернуться";
+}
+
+public static class BotCommands
+{
+    public const string Start = "/start";
+    public const string AddDictionary = "/add-dictionary";
+    public const string AddDictionaryClick = "/add-dictionary-click";
+    public const string SelectDictionary = "/select-dictionary";
+    public const string Next = "/forward";
+    public const string Previous = "/back";
+    public const string Return = "/return";
+}
--- ENGHelperBot/BotCommands.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ENGHelperBot;
-
-public static class BotCommands
-{
-    public const string Start = "/start";
-    public const string MyDictionaries = "📚 Мои словари";
-    public const string AddWord = "➕ Добавить слово";
-    public const string TakeTest = "📝 Пройти тест";
-}
