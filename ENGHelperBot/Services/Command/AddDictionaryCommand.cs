21ef8d2a04227cb7549e5a6ee3c1fb474b99df08
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-27 18:47:15 +0300

    Add AddDictionaryCommand handler

---
+++ ENGHelperBot/Services/Command/AddDictionaryCommand.cs
@@ -0,0 +1,34 @@
+﻿using ENGHelperBot.Services.Context;
+using ENGHelperBot.Services.Repositories.Dictionaries;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ENGHelperBot.Services.Command;
+
+public class AddDictionaryCommand(IServiceScopeFactory scopeFactory) : ICommandHandler
+{
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
+    public async Task<Message> HandleAsync(ITelegramBotClient bot, Update update)
+    {
+        const string dictCreatedMsg = """
+            🎉 <b>Новый словарь создан!</b>
+        """;
+
+        const string dictExistsMsg = """
+            ⚠️ <b>Такой словарь уже есть!</b>
+        """;
+
+        using var scope = _scopeFactory.CreateScope();
+
+        var chatContext = scope.ServiceProvider.GetRequiredService<IChatContextProvider>();
+        chatContext.ResetFollowingCommand(update.Message!.Chat.Id);
+        
+        var dictionaryService = scope.ServiceProvider.GetRequiredService<IDictionaryRepository>();
+        var isSuccess = await dictionaryService.CreateAsync(new() { Name = update.Message!.Text!, UserId = update.Message.Chat.Id },
+                                                            d => d.UserId == update.Message.Chat.Id);
+
+        return await bot.SendMessage(update.Message.Chat, isSuccess ? dictCreatedMsg : dictExistsMsg, parseMode: ParseMode.Html);
+    }
+}
