da0b44352209dc96ee50d97962d96b5a623d3fba
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-27 18:46:55 +0300

    Add AddDictionaryClickCommand handler

---
+++ ENGHelperBot/Services/Command/AddDictionaryClickCommand.cs
@@ -0,0 +1,24 @@
+﻿using ENGHelperBot.Services.Context;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ENGHelperBot.Services.Command;
+
+public class AddDictionaryClickCommand(IServiceScopeFactory scopeFactory) : ICommandHandler
+{
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
+    public async Task<Message> HandleAsync(ITelegramBotClient bot, Update update)
+    {
+        const string message = """
+            🎨 <b>Дайте название вашему новому словарю:</b>
+        """;
+
+        using var scope = _scopeFactory.CreateScope();
+        var chatContext = scope.ServiceProvider.GetRequiredService<IChatContextProvider>();
+        chatContext.SetFollowingCommand(update.CallbackQuery!.Message!.Chat.Id, BotCommands.AddDictionary);
+
+        return await bot.SendMessage(update.CallbackQuery!.Message.Chat, message, parseMode: ParseMode.Html);
+    }
+}
