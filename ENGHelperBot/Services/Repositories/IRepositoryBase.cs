56b65d86283c14ca5c589e5eef99d090ea96a90a
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-04-27 18:41:51 +0300

    Fix signature mismatch of CreateAsync

--- ENGHelperBot/Services/Repositories/IRepositoryBase.cs
+++ ENGHelperBot/Services/Repositories/IRepositoryBase.cs
@@ -5,7 +5,7 @@ namespace ENGHelperBot.Services.Repositories;
 
 public interface IRepositoryBase<T>
 {
-    ValueTask CreateAsync(T entity, Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
+    ValueTask<bool> CreateAsync(T entity, Expression<Func<T, bool>> existenceExpression, CancellationToken cancellationToken = default);
     ValueTask UpdateAsync(T entity, CancellationToken cancellationToken = default);
     ValueTask DeleteAsync(T entity, CancellationToken cancellationToken = default);
     ValueTask<T?> FindAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
