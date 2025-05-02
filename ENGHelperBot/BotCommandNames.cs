99dfccc0ef372b00385eb254bedb9159baeb0c74
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-05-02 13:30:24 +0300

    Add new command names and shortened old ones

--- ENGHelperBot/BotCommandNames.cs
+++ ENGHelperBot/BotCommandNames.cs
@@ -3,8 +3,10 @@
 public static class BotCommandTexts
 {
     public const string MyDictionaries = "📚 Мои словари";
-    public const string AddWord = "➕ Добавить слово";
     public const string AddDictionary = "➕ Добавить словарь";
+    public const string RenameDictionary = "✏️ Переименовать словарь";
+    public const string RemoveDictionary = "❌ Удалить словарь";
+    public const string AddWord = "➕ Добавить слово";
     public const string TakeTest = "📝 Пройти тест";
     public const string Back = "Назад";
     public const string Forward = "Вперед";
@@ -14,9 +16,13 @@ public static class BotCommandTexts
 public static class BotCommands
 {
     public const string Start = "/start";
-    public const string AddDictionary = "/add-dictionary";
-    public const string AddDictionaryClick = "/add-dictionary-click";
-    public const string SelectDictionary = "/select-dictionary";
+    public const string AddDictionary = "/add-dict";
+    public const string AddDictionaryClick = "/add-dict-click";
+    public const string SelectDictionary = "/select-dict";
+    public const string RenameDictionary = "/rename-dict";
+    public const string RenameDictionaryClick = "/rename-dict-click";
+    public const string RemoveDictionary = "/delete-dict";
+    public const string SelectWord = "/select-word";
     public const string Next = "/forward";
     public const string Previous = "/back";
     public const string Return = "/return";
