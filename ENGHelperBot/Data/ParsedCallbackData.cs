474877e9aca0a2b45701ea9d42f936f8d1cd7061
AlexxanndR <alexandrov.alexandder@gmail.com>
2025-05-02 13:33:05 +0300

    Add SelectionData record

--- ENGHelperBot/Data/ParsedCallbackData.cs
+++ ENGHelperBot/Data/ParsedCallbackData.cs
@@ -3,9 +3,12 @@
 public record ParsedCallbackData
 {
     public PaginationData? PaginationData { get; set; }
+    public SelectionData? SelectionData { get; set; }
 }
 
 public record PaginationData(PaginationData.DataType Type, int CurrentPage, int MessageId)
 {
     public enum DataType { Dictionary, Word };
 }
+
+public record SelectionData(long Id);
